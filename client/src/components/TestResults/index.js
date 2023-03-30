1ca85c72df95ef4b1fdd8158f1c59abc7fad7a95
HP 6 <Shamoointhedark@gmail.com>
2023-03-30 14:11:06 +0500

    hade/show test results table

--- client/src/components/TestResults/index.js
+++ client/src/components/TestResults/index.js
@@ -4,6 +4,7 @@ import { fetchAllTestResults, removeTestResult } from "../../redux/slices/answer
 import styles from "./TestResults.module.scss";
 
 function TestResults() {
+  const [hide, setHide] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -20,6 +21,9 @@ function TestResults() {
   return (
     <div className={styles.container}>
       <h2>Результаты тестирования</h2>
+      <button onClick={() => setHide(!hide)} className={styles.toggleTable}>
+        {!hide ? "Свернуть" : "Развернуть"}
+      </button>
       {testStatus == "loading" ? (
         <div className={styles.grid}>
           <div className={styles.gridHead}>Урок</div>
@@ -38,28 +42,30 @@ function TestResults() {
           </div>
         </div>
       ) : (
-        <div className={styles.grid}>
-          <div className={styles.gridHead}>Урок</div>
-          <div className={styles.gridHead}>Ученик</div>
-          <div className={styles.gridHead}>Всего</div>
-          <div className={styles.gridHead}>Правильно</div>
-          <div className={styles.gridHead}>Результат</div>
-          <div className={styles.gridHead}>Действие</div>
-          {testResults.map((result) => {
-            return (
-              <Fragment key={result._id}>
-                <div>{result.lessonId.title}</div>
-                <div>{result.userId.firstName + " " + result.userId.lastName}</div>
-                <div>{result.questionCounter}</div>
-                <div>{result.rightAnswers}</div>
-                <div>{result.userScore}%</div>
-                <div>
-                  <p onClick={() => removeResult(result._id)}>Удалить</p>
-                </div>
-              </Fragment>
-            );
-          })}
-        </div>
+        !hide && (
+          <div className={styles.grid}>
+            <div className={styles.gridHead}>Урок</div>
+            <div className={styles.gridHead}>Ученик</div>
+            <div className={styles.gridHead}>Всего</div>
+            <div className={styles.gridHead}>Правильно</div>
+            <div className={styles.gridHead}>Результат</div>
+            <div className={styles.gridHead}>Действие</div>
+            {testResults.map((result) => {
+              return (
+                <Fragment key={result._id}>
+                  <div>{result.lessonId.title}</div>
+                  <div>{result.userId.firstName + " " + result.userId.lastName}</div>
+                  <div>{result.questionCounter}</div>
+                  <div>{result.rightAnswers}</div>
+                  <div>{result.userScore}%</div>
+                  <div>
+                    <p onClick={() => removeResult(result._id)}>Удалить</p>
+                  </div>
+                </Fragment>
+              );
+            })}
+          </div>
+        )
       )}
     </div>
   );
